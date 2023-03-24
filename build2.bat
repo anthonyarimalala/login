bc30ce6a3652617e0336b82849f9e3aaf1608caf
anthonyarimalala <anthonyarimalala@gmail.com>
2023-03-24 08:32:15 +0100

    automatisation build2.bat

---
+++ build2.bat
@@ -0,0 +1,14 @@
+cd framework
+    javac -d . FrontServlet.java
+    javac -d . Mapping.java
+    javac -d . url.java
+    javac -d . *.java
+    jar -cf fw.jar .
+    copy fw.jar "D:\Devoirs\MrNaina\framework1\test-framework\WEB-INF\lib"
+
+cd ..
+
+cd test-framework/
+    jar -cf test-servlet.war *
+    copy "test-servlet.war" "C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\test-framework.war"
+cd ..
