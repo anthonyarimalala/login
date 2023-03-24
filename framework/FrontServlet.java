5c65ad7ce0d9692a35fd4575ba16a4a216ba2819
anthonyarimalala <anthonyarimalala@gmail.com>
2023-03-24 08:21:44 +0100

    sprint3 an

--- framework/FrontServlet.java
+++ framework/FrontServlet.java
@@ -24,6 +24,27 @@ import java.lang.reflect.*;
 public class FrontServlet extends HttpServlet{ 
     HashMap<String,Mapping> mappingUrls;
 
+    public void init() throws ServletException {
+        try {
+        mappingUrls = new HashMap<String, Mapping>();
+        String packageName = "etu2033.model";
+        URL root = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "//")); 
+            for (File file : new File(root.getFile().replaceAll("%20", " ")).listFiles()) {
+                if (file.getName().contains(".class")) {
+                    String className = file.getName().replaceAll(".class$", "");
+                    Class<?> cls = Class.forName(packageName + "." + className);
+                    for (Method method : cls.getDeclaredMethods()) {
+                        if (method.isAnnotationPresent(url.class)) {
+                            mappingUrls.put(method.getAnnotation(url.class).value(), new Mapping(cls.getName(), method.getName()));
+                        }
+                    }
+                }
+            }
+            } catch (Exception e) {
+                throw new ServletException(e);
+            }
+    } 
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
